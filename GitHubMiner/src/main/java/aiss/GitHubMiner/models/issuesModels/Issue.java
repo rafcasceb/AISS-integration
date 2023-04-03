4f1ef75215f4ee31f167d7f0664bed1b18dc2604
raulherasperez <93604567+raulherasperez@users.noreply.github.com>
2023-04-03 13:50:35 +0200

    Add files via upload

---
+++ GitHubMiner/src/main/java/aiss/GitHubMiner/models/issuesModels/Issue.java
@@ -0,0 +1,372 @@
+
+package aiss.GitHubMiner.models.issuesModels;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "node_id",
+    "url",
+    "repository_url",
+    "labels_url",
+    "comments_url",
+    "events_url",
+    "html_url",
+    "number",
+    "state",
+    "title",
+    "body",
+    "user",
+    "labels",
+    "assignee",
+    "assignees",
+    "milestone",
+    "locked",
+    "active_lock_reason",
+    "comments",
+    "pull_request",
+    "closed_at",
+    "created_at",
+    "updated_at",
+    "repository",
+    "author_association"
+})
+@Generated("jsonschema2pojo")
+public class Issue {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("node_id")
+    private String nodeId;
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("repository_url")
+    private String repositoryUrl;
+    @JsonProperty("labels_url")
+    private String labelsUrl;
+    @JsonProperty("comments_url")
+    private String commentsUrl;
+    @JsonProperty("events_url")
+    private String eventsUrl;
+    @JsonProperty("html_url")
+    private String htmlUrl;
+    @JsonProperty("number")
+    private Integer number;
+    @JsonProperty("state")
+    private String state;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("body")
+    private String body;
+    @JsonProperty("user")
+    private User user;
+    @JsonProperty("labels")
+    private List<Label> labels;
+    @JsonProperty("assignee")
+    private Assignee assignee;
+    @JsonProperty("assignees")
+    private List<Assignee__1> assignees;
+    @JsonProperty("milestone")
+    private Milestone milestone;
+    @JsonProperty("locked")
+    private Boolean locked;
+    @JsonProperty("active_lock_reason")
+    private String activeLockReason;
+    @JsonProperty("comments")
+    private Integer comments;
+    @JsonProperty("pull_request")
+    private PullRequest pullRequest;
+    @JsonProperty("closed_at")
+    private Object closedAt;
+    @JsonProperty("created_at")
+    private String createdAt;
+    @JsonProperty("updated_at")
+    private String updatedAt;
+    @JsonProperty("repository")
+    private Repository repository;
+    @JsonProperty("author_association")
+    private String authorAssociation;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("node_id")
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    @JsonProperty("node_id")
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonProperty("repository_url")
+    public String getRepositoryUrl() {
+        return repositoryUrl;
+    }
+
+    @JsonProperty("repository_url")
+    public void setRepositoryUrl(String repositoryUrl) {
+        this.repositoryUrl = repositoryUrl;
+    }
+
+    @JsonProperty("labels_url")
+    public String getLabelsUrl() {
+        return labelsUrl;
+    }
+
+    @JsonProperty("labels_url")
+    public void setLabelsUrl(String labelsUrl) {
+        this.labelsUrl = labelsUrl;
+    }
+
+    @JsonProperty("comments_url")
+    public String getCommentsUrl() {
+        return commentsUrl;
+    }
+
+    @JsonProperty("comments_url")
+    public void setCommentsUrl(String commentsUrl) {
+        this.commentsUrl = commentsUrl;
+    }
+
+    @JsonProperty("events_url")
+    public String getEventsUrl() {
+        return eventsUrl;
+    }
+
+    @JsonProperty("events_url")
+    public void setEventsUrl(String eventsUrl) {
+        this.eventsUrl = eventsUrl;
+    }
+
+    @JsonProperty("html_url")
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    @JsonProperty("html_url")
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    @JsonProperty("number")
+    public Integer getNumber() {
+        return number;
+    }
+
+    @JsonProperty("number")
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @JsonProperty("state")
+    public String getState() {
+        return state;
+    }
+
+    @JsonProperty("state")
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("body")
+    public String getBody() {
+        return body;
+    }
+
+    @JsonProperty("body")
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @JsonProperty("user")
+    public User getUser() {
+        return user;
+    }
+
+    @JsonProperty("user")
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @JsonProperty("labels")
+    public List<Label> getLabels() {
+        return labels;
+    }
+
+    @JsonProperty("labels")
+    public void setLabels(List<Label> labels) {
+        this.labels = labels;
+    }
+
+    @JsonProperty("assignee")
+    public Assignee getAssignee() {
+        return assignee;
+    }
+
+    @JsonProperty("assignee")
+    public void setAssignee(Assignee assignee) {
+        this.assignee = assignee;
+    }
+
+    @JsonProperty("assignees")
+    public List<Assignee__1> getAssignees() {
+        return assignees;
+    }
+
+    @JsonProperty("assignees")
+    public void setAssignees(List<Assignee__1> assignees) {
+        this.assignees = assignees;
+    }
+
+    @JsonProperty("milestone")
+    public Milestone getMilestone() {
+        return milestone;
+    }
+
+    @JsonProperty("milestone")
+    public void setMilestone(Milestone milestone) {
+        this.milestone = milestone;
+    }
+
+    @JsonProperty("locked")
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    @JsonProperty("locked")
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
+    @JsonProperty("active_lock_reason")
+    public String getActiveLockReason() {
+        return activeLockReason;
+    }
+
+    @JsonProperty("active_lock_reason")
+    public void setActiveLockReason(String activeLockReason) {
+        this.activeLockReason = activeLockReason;
+    }
+
+    @JsonProperty("comments")
+    public Integer getComments() {
+        return comments;
+    }
+
+    @JsonProperty("comments")
+    public void setComments(Integer comments) {
+        this.comments = comments;
+    }
+
+    @JsonProperty("pull_request")
+    public PullRequest getPullRequest() {
+        return pullRequest;
+    }
+
+    @JsonProperty("pull_request")
+    public void setPullRequest(PullRequest pullRequest) {
+        this.pullRequest = pullRequest;
+    }
+
+    @JsonProperty("closed_at")
+    public Object getClosedAt() {
+        return closedAt;
+    }
+
+    @JsonProperty("closed_at")
+    public void setClosedAt(Object closedAt) {
+        this.closedAt = closedAt;
+    }
+
+    @JsonProperty("created_at")
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    @JsonProperty("created_at")
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @JsonProperty("updated_at")
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @JsonProperty("updated_at")
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @JsonProperty("repository")
+    public Repository getRepository() {
+        return repository;
+    }
+
+    @JsonProperty("repository")
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
+
+    @JsonProperty("author_association")
+    public String getAuthorAssociation() {
+        return authorAssociation;
+    }
+
+    @JsonProperty("author_association")
+    public void setAuthorAssociation(String authorAssociation) {
+        this.authorAssociation = authorAssociation;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ GitHubMiner/src/main/java/aiss/GitHubMiner/models/issuesModels/License.java
@@ -0,0 +1,111 @@
+
+package aiss.GitHubMiner.models.issuesModels;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "key",
+    "name",
+    "url",
+    "spdx_id",
+    "node_id",
+    "html_url"
+})
+@Generated("jsonschema2pojo")
+public class License {
+
+    @JsonProperty("key")
+    private String key;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("spdx_id")
+    private String spdxId;
+    @JsonProperty("node_id")
+    private String nodeId;
+    @JsonProperty("html_url")
+    private String htmlUrl;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("key")
+    public String getKey() {
+        return key;
+    }
+
+    @JsonProperty("key")
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonProperty("spdx_id")
+    public String getSpdxId() {
+        return spdxId;
+    }
+
+    @JsonProperty("spdx_id")
+    public void setSpdxId(String spdxId) {
+        this.spdxId = spdxId;
+    }
+
+    @JsonProperty("node_id")
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    @JsonProperty("node_id")
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    @JsonProperty("html_url")
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    @JsonProperty("html_url")
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
