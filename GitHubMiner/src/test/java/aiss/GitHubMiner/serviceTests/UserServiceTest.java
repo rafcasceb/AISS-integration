9667ef43c6aacf631201a2b840ec89cde9f0023e
Rafael Castillo Cebolla <93669913+rafcasceb@users.noreply.github.com>
2023-04-14 00:44:44 +0200

    Added asserts to UserServiceTest

--- GitHubMiner/src/test/java/aiss/GitHubMiner/serviceTests/UserServiceTest.java
+++ GitHubMiner/src/test/java/aiss/GitHubMiner/serviceTests/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UserServiceTest {
@@ -17,6 +18,16 @@ class UserServiceTest {
     @DisplayName("Get user")
     void getUsers(){
         User user = service.getUser("rafcasceb");
-        System.out.println(user.getName());
+        assertEquals(user.getName().trim(), "Rafael Castillo Cebolla", "Name doesn't match.");
     }
+
+    /*
+    @Test
+    @DisplayName("Post user to GitMiner from GitHub")
+    void postUserTest(){
+        User user = service.postUser("rafcasceb");
+        // If the post works, it will return the posted user
+        assertEquals(user.getName().trim(), "Rafael Castillo Cebolla", "Name doesn't match.");
+    }
+    */
 }
