d84b5daf4db78b9e822062979ff9d6c5cb9b6147
Rafael Castillo Cebolla <93669913+rafcasceb@users.noreply.github.com>
2023-04-14 00:46:04 +0200

    Added asserts to UserServiceTest

--- GitLabMiner/src/test/java/aiss/GitLabMiner/serviceTests/UserServiceTest.java
+++ GitLabMiner/src/test/java/aiss/GitLabMiner/serviceTests/UserServiceTest.java
@@ -7,16 +7,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class UserServiceTest {
 
     @Autowired
     UserService service;
 
+    String token = "glpat-EWrMxiW1vhazpsMAsc4A";
+
     @Test
     @DisplayName("Get user test")
     void getUser(){
-        User user = service.getUser("jack_smith", "glpat-EWrMxiW1vhazpsMAsc4A");
-        System.out.println(user.getName());
+        User user = service.getUser("jack_smith", token);
+        assertEquals(user.getName().trim(), "Jack Smith", "Name doesn't match.");
     }
+
+    /*
+    @Test
+    @DisplayName("TEST - Post user to GitMiner from GitLab")
+    void postUserTest(){
+        User user = service.postUser("jack_smith", token);
+        // If the post works, it will return the posted user
+        assertEquals(user.getName().trim(), "Jack Smith", "Name doesn't match.");
+    }
+    */
 }
