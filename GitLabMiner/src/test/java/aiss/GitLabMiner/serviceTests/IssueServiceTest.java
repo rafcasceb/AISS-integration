1d609821de888fff43318ada2b0c7226c7538d3d
raulherasperez <93604567+raulherasperez@users.noreply.github.com>
2023-04-16 17:31:10 +0200

    Add files via upload

--- GitLabMiner/src/test/java/aiss/GitLabMiner/serviceTests/IssueServiceTest.java
+++ GitLabMiner/src/test/java/aiss/GitLabMiner/serviceTests/IssueServiceTest.java
@@ -1,5 +1,6 @@
 package aiss.GitLabMiner.serviceTests;
 
+import aiss.GitLabMiner.Models.Commits.Commit;
 import aiss.GitLabMiner.Models.Issues.Issue;
 import aiss.GitLabMiner.Service.*;
 import org.junit.jupiter.api.DisplayName;
@@ -7,7 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class IssueServiceTest {
@@ -31,13 +36,27 @@ public class IssueServiceTest {
     @Test
     @DisplayName( "Get issues with pagination test")
     void getIssuesPagination() {
+        String id = "4207231";
+        Integer since = 900;
+        Integer maxPages = 10;
+        Integer contentLimit = maxPages*30;
+
         List<Issue> issues = service
-                .getIssuesPagination("17960074","glpat-EWrMxiW1vhazpsMAsc4A",null,null
-                );
+                .getIssuesPagination(id,"glpat-EWrMxiW1vhazpsMAsc4A",
+                        since, maxPages);
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        LocalDateTime limit = LocalDateTime.now().minusDays(since);
 
         for(Issue i : issues){
-            System.out.println(i.getCreatedAt());
-        }
+            LocalDateTime issueDate = LocalDateTime
+                    .parse(i.getCreatedAt(), formatter);
 
+            //System.out.println(c.getCommittedDate());
+            assertEquals(issueDate.isAfter(limit), true, "Issue date before limit");
+
+        }
+        assertEquals(issues.size()<=contentLimit, true, "Page limit exceeded");
     }
 }
