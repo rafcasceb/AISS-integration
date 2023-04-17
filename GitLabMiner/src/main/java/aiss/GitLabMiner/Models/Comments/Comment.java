aab31e8a0eca3ee852aff229120a4c7f283b076b
Andresmarevi <93730851+Andresmarevi@users.noreply.github.com>
2023-04-17 13:07:19 +0200

    Add files via upload

--- GitLabMiner/src/main/java/aiss/GitLabMiner/Models/Comments/Comment.java
+++ GitLabMiner/src/main/java/aiss/GitLabMiner/Models/Comments/Comment.java
@@ -13,27 +13,83 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "note",
-    "author"
+    "id",
+    "body",
+    "attachment",
+    "author",
+    "created_at",
+    "updated_at",
+    "system",
+    "noteable_id",
+    "noteable_type",
+    "project_id",
+    "noteable_iid",
+    "resolvable",
+    "confidential",
+    "internal"
 })
 @Generated("jsonschema2pojo")
 public class Comment {
 
-    @JsonProperty("note")
-    private String note;
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("body")
+    private String body;
+    @JsonProperty("attachment")
+    private Object attachment;
     @JsonProperty("author")
     private Author author;
+    @JsonProperty("created_at")
+    private String createdAt;
+    @JsonProperty("updated_at")
+    private String updatedAt;
+    @JsonProperty("system")
+    private Boolean system;
+    @JsonProperty("noteable_id")
+    private Integer noteableId;
+    @JsonProperty("noteable_type")
+    private String noteableType;
+    @JsonProperty("project_id")
+    private Integer projectId;
+    @JsonProperty("noteable_iid")
+    private Integer noteableIid;
+    @JsonProperty("resolvable")
+    private Boolean resolvable;
+    @JsonProperty("confidential")
+    private Boolean confidential;
+    @JsonProperty("internal")
+    private Boolean internal;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
 
-    @JsonProperty("note")
-    public String getNote() {
-        return note;
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
     }
 
-    @JsonProperty("note")
-    public void setNote(String note) {
-        this.note = note;
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("body")
+    public String getBody() {
+        return body;
+    }
+
+    @JsonProperty("body")
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @JsonProperty("attachment")
+    public Object getAttachment() {
+        return attachment;
+    }
+
+    @JsonProperty("attachment")
+    public void setAttachment(Object attachment) {
+        this.attachment = attachment;
     }
 
     @JsonProperty("author")
@@ -44,6 +100,106 @@ public class Comment {
     @JsonProperty("author")
     public void setAuthor(Author author) {
         this.author = author;
+    }
+
+    @JsonProperty("created_at")
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    @JsonProperty("created_at")
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @JsonProperty("updated_at")
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @JsonProperty("updated_at")
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @JsonProperty("system")
+    public Boolean getSystem() {
+        return system;
+    }
+
+    @JsonProperty("system")
+    public void setSystem(Boolean system) {
+        this.system = system;
+    }
+
+    @JsonProperty("noteable_id")
+    public Integer getNoteableId() {
+        return noteableId;
+    }
+
+    @JsonProperty("noteable_id")
+    public void setNoteableId(Integer noteableId) {
+        this.noteableId = noteableId;
+    }
+
+    @JsonProperty("noteable_type")
+    public String getNoteableType() {
+        return noteableType;
+    }
+
+    @JsonProperty("noteable_type")
+    public void setNoteableType(String noteableType) {
+        this.noteableType = noteableType;
+    }
+
+    @JsonProperty("project_id")
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    @JsonProperty("project_id")
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    @JsonProperty("noteable_iid")
+    public Integer getNoteableIid() {
+        return noteableIid;
+    }
+
+    @JsonProperty("noteable_iid")
+    public void setNoteableIid(Integer noteableIid) {
+        this.noteableIid = noteableIid;
+    }
+
+    @JsonProperty("resolvable")
+    public Boolean getResolvable() {
+        return resolvable;
+    }
+
+    @JsonProperty("resolvable")
+    public void setResolvable(Boolean resolvable) {
+        this.resolvable = resolvable;
+    }
+
+    @JsonProperty("confidential")
+    public Boolean getConfidential() {
+        return confidential;
+    }
+
+    @JsonProperty("confidential")
+    public void setConfidential(Boolean confidential) {
+        this.confidential = confidential;
+    }
+
+    @JsonProperty("internal")
+    public Boolean getInternal() {
+        return internal;
+    }
+
+    @JsonProperty("internal")
+    public void setInternal(Boolean internal) {
+        this.internal = internal;
     }
 
     @JsonAnyGetter
