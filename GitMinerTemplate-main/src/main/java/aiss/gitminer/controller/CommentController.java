b97c434bab85c1c2bdc2952eb9cd9111fff28946
raulherasperez <93604567+raulherasperez@users.noreply.github.com>
2023-04-23 14:53:01 +0200

    Add files via upload

--- GitMinerTemplate-main/src/main/java/aiss/gitminer/controller/CommentController.java
+++ GitMinerTemplate-main/src/main/java/aiss/gitminer/controller/CommentController.java
@@ -1,4 +1,63 @@
 package aiss.gitminer.controller;
 
+import aiss.gitminer.model.Comment;
+import aiss.gitminer.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.List;
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
+    @Autowired
+    CommentRepository repository;
+
+    @GetMapping
+    public List<Comment> findAll(){return repository.findAll();}
+
+    @GetMapping("/{id}")
+    public Comment findOne(@PathVariable long id){
+        Optional<Comment> comment = repository.findById(id);
+        return comment.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Comment create (@Valid @RequestBody Comment comment){
+        Comment _comment = repository
+                .save(new Comment(comment.getId(),
+                        comment.getBody(),
+                        comment.getAuthor(),
+                        comment.getCreatedAt(),
+                        comment.getUpdatedAt()));
+        return _comment;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Comment updated,@PathVariable long id){
+        Optional<Comment> commentData = repository.findById(id);
+
+        Comment _comment = commentData.get();
+        _comment.setId(updated.getId());
+        _comment.setBody(updated.getBody());
+        _comment.setAuthor(updated.getAuthor());
+        _comment.setCreatedAt(updated.getCreatedAt());
+        _comment.setUpdatedAt(updated.getUpdatedAt());
+
+        repository.save(_comment);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete (@PathVariable long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }
+    }
 }
--- GitMinerTemplate-main/src/main/java/aiss/gitminer/controller/ProjectController.java
+++ GitMinerTemplate-main/src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -1,4 +1,61 @@
 package aiss.gitminer.controller;
 
+import aiss.gitminer.model.Project;
+import aiss.gitminer.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/projects")
 public class ProjectController {
+    @Autowired
+    ProjectRepository repository;
+
+    @GetMapping
+    public List<Project> findAll(){return repository.findAll();}
+
+    @GetMapping("/{id}")
+    public Project findOne(@PathVariable long id){
+        Optional<Project> project = repository.findById(id);
+        return project.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Project project (@Valid @RequestBody Project project){
+        Project _project = repository
+                .save(new Project(project.getId(),
+                        project.getName(),
+                        project.getWebUrl(),
+                        project.getCommits(),
+                        project.getIssues()));
+        return _project;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Project updated,@PathVariable long id){
+        Optional<Project> projectData = repository.findById(id);
+
+        Project _project = projectData.get();
+        _project.setId(updated.getId());
+        _project.setName(updated.getName());
+        _project.setWebUrl(updated.getWebUrl());
+        _project.setCommits(updated.getCommits());
+        _project.setIssues(updated.getIssues());
+        repository.save(_project);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }
+    }
 }
