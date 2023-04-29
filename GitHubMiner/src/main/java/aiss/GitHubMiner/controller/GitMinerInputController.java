77508e3f256191e415f1d8c77b1ce70447c3519b
Rafael Castillo Cebolla <93669913+rafcasceb@users.noreply.github.com>
2023-04-29 18:39:42 +0200

    GitMinerInputController

---
+++ GitHubMiner/src/main/java/aiss/GitHubMiner/controller/GitMinerInputController.java
@@ -0,0 +1,48 @@
+package aiss.GitHubMiner.controller;
+
+import aiss.GitHubMiner.models.commitsModels.Commit;
+import aiss.GitHubMiner.models.GitMinerInput.CommitInput;
+import aiss.GitHubMiner.services.CommitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/commits")
+
+public class GitMinerInputController {
+
+    @Autowired
+    CommitService Commitservice;
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    @GetMapping("/{id}")
+    public List<Commit> findAll (@PathVariable String id){
+        List<Commit> commits = Commitservice.getCommits(id);
+        return  commits;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{id}")
+    public CommitInput create (@PathVariable String id){
+        List<Commit> commits = Commitservice.getCommits(id);
+        Commit commit = commits.get(0);
+        CommitInput commitInput = new CommitInput(commit);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String url = "http://localhost:8080/api/commits";
+        HttpEntity<CommitInput> request =
+                new HttpEntity<CommitInput>(commitInput, headers);
+        restTemplate.postForObject(url, request, CommitInput.class);
+        return  commitInput;
+    }
+}
