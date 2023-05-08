53d7c0cd843b3a798c3c533de7d52cf0bf6b07e3
raulherasperez <93604567+raulherasperez@users.noreply.github.com>
2023-05-08 17:32:26 +0200

    Add files via upload

--- GitMiner-29_04/src/main/java/aiss/gitminer/repository/CommentRepository.java
+++ GitMiner-29_04/src/main/java/aiss/gitminer/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package aiss.gitminer.repository;
 
 import aiss.gitminer.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository  extends JpaRepository <Comment, String> {
+
+    Page<Comment> findByAuthor(String author, Pageable pageable);
 }
